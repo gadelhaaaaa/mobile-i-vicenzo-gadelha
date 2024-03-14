f8e272e05a2824ca94442a6bd2e9ef761f107291
gadelha <vicenzoggreco@gmail.com>
2024-03-14 08:21:38 -0300

    criaçao auula 03

---
+++ aula 03/funcoes1.js
@@ -0,0 +1,14 @@
+function Soma () {
+    let n1 = 2;
+    let n2 = 6;
+
+    console.log(n1 + n2);
+}
+
+Soma()
+
+function SomaComParametros (n1, n2) {
+    console.log(n1 + n2)
+}
+
+SomaComParametros(8, 10)
---
+++ aula 03/metodo.js
@@ -0,0 +1,11 @@
+let txt = "Hello Amigos";
+
+console.log(txt.length);
+
+console.log(txt.slice(6, 12));
+
+console.log(txt.replace("Hello", "pika dura"));
+
+let newTxt = txt.replace("Hello", "pirocona");
+
+console.warn(newTxt.toUpperCase())
