7ede5be913ca90ade9feab5605855d2bcff3eb07
gadelhaaaaa <160802167+gadelhaaaaa@users.noreply.github.com>
2024-06-20 07:51:39 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,24 @@
+var numero = 2;
+let texto = "Hello";
+const oQueEisso = "?";
+
+// Oponto e virgula é opcional! Boas praticas o usam 
+
+console.log('Tipo var:', numero);
+// Concatenar com virgula me garante um espaço
+
+numero = 13;
+
+console.log(texto + " o numero agora é " + numero)
+
+texto = 69
+
+console.log(texto + numero)
+// Concatenar com + dois valores numericos vai acabar somando
+
+// console.log(oQueEisso)
+// Constantes nao podem ter novos valores assinalados a ela
+
+
+// oQueEisso = "uma constante?????"
+// console.log(oQueEisso)
